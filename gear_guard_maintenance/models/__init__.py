c13646d04eb4a7ff2b538fcc4381c3509c3809aa
Palak <palakthummar7080@gmail.com>
2025-12-27 12:03:38 +0530

    chore: remove existing codebase for fresh start

--- gear_guard_maintenance/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import team
-from . import category
-from . import stage
-from . import equipment
-from . import request
--- gear_guard_maintenance/models/category.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class GgEquipmentCategory(models.Model):
-    _name = 'gg.equipment.category'
-    _description = 'Equipment Category'
-
-    name = fields.Char(required=True)
